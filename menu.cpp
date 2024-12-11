cb49dbdbce78ef1d6ef2a71fefc5ae1f4042087e
dani-dev-lab <m.hamdani160305@gmail.com>
2024-12-11 11:54:15 +0800

    u: penyesuaian isi pada halaman depan

--- menu.cpp
+++ menu.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 void dMenu(){
 system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
+cout<<"Aplikasi Sorting Bubble"<<"\n";       
+cout<<"1. masukkan data"<<"\n";            
+cout<<"2. tampilkan data"<<"\n";            
+cout<<"3. sorting asc"<<"\n";           
+cout<<"4. sorting dsc"<<"\n";            
 cout<<"5. Exit"<<"\n";           
 cout<<"Masukan angka :";        
 
