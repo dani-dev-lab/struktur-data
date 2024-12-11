bf9aaf1a92f1ac8c305cc0b80457997df4816fd9
dani-dev-lab <m.hamdani160305@gmail.com>
2024-12-11 12:02:39 +0800

    perubahan pada coding

--- menu.cpp
+++ menu.cpp
@@ -2,62 +2,95 @@
 #include <conio.h>
 using namespace std;
 
+int n; // Variabel global untuk menyimpan jumlah elemen
 
-void dMenu(){
-system("cls");
-cout<<"Aplikasi Sorting Bubble"<<"\n";       
-cout<<"1. masukkan data"<<"\n";            
-cout<<"2. tampilkan data"<<"\n";            
-cout<<"3. sorting asc"<<"\n";           
-cout<<"4. sorting dsc"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
-
+void tukar(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
+void dMenu() {
+    system("cls");
+    cout << "Aplikasi Sorting Bubble" << "\n";
+    cout << "1. Masukkan data" << "\n";
+    cout << "2. Tampilkan data" << "\n";
+    cout << "3. Sorting ASC" << "\n";
+    cout << "4. Sorting DSC" << "\n";
+    cout << "5. Exit" << "\n";
+    cout << "Masukkan angka: ";
 }
 
+void mPertama(string pesan) {
+    system("cls");
+    cout << "Halo, saya menu " << pesan;
+    getch();
+}
 
 int main() {
-char pl;
-do
-{
-    dMenu();
-    pl=getch();
-  switch (pl)
-  {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    mPertama("ke- empat");
-    /* code */
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
-  }
+    int data[100]; // Variabel lokal untuk menyimpan data array
+    char pl;
 
+    do {
+        dMenu();
+        pl = getch();
+        switch (pl) {
+            case '1':
+                system("cls");
+                cout << "Masukkan jumlah data: ";
+                cin >> n;
+                if (n > 100) {
+                    cout << "Jumlah data tidak boleh lebih dari 100.\n";
+                    n = 100;
+                }
+                for (int i = 0; i < n; i++) {
+                    cout << "Masukkan elemen ke-" << i + 1 << ": ";
+                    cin >> data[i];
+                }
+                break;
+            case '2':
+                system("cls");
+                cout << "Data yang dimasukkan: \n";
+                for (int i = 0; i < n; i++) {
+                    cout << data[i] << " ";
+                }
+                getch();
+                break;
+            case '3':
+                system("cls");
+                // Sorting ASC (Bubble Sort)
+                for (int i = 0; i < n - 1; i++) {
+                    for (int j = 0; j < n - i - 1; j++) {
+                        if (data[j] > data[j + 1]) {
+                            tukar(&data[j], &data[j + 1]);
+                        }
+                    }
+                }
+                cout << "Data telah diurutkan secara ASC.\n";
+                getch();
+                break;
+            case '4':
+                system("cls");
+                // Sorting DSC (Bubble Sort)
+                for (int i = 0; i < n - 1; i++) {
+                    for (int j = 0; j < n - i - 1; j++) {
+                        if (data[j] < data[j + 1]) {
+                            tukar(&data[j], &data[j + 1]);
+                        }
+                    }
+                }
+                cout << "Data telah diurutkan secara DSC.\n";
+                getch();
+                break;
+            case '5':
+                break;
+            default:
+                system("cls");
+                cout << "Pilihan Tidak Tersedia";
+                getch();
+                break;
+        }
+    } while (pl != '5');
 
-} while (pl!='5');
-  return 0;
+    return 0;
 }
